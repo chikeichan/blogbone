376d37bd9de550d39a60fa91f8422f6e062b7719
Chi Kei Chan <chikeichan@gmail.com>
2014-10-31 14:35:02 -0700

    Added collection model and view; Added + icon

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var BlogModel = Backbone.Model.extend({
 	}
 });
 
-var BlogsList = Backbone.Collection.extend({
+var BlogList = Backbone.Collection.extend({
 	model: BlogModel,
 	localStorage: new Backbone.LocalStorage('blogboneDB'),
 	comparator: 'fullName'
 });
 
-var BlogView = Backbone.View.extend({
+var BlogSnapShot = Backbone.View.extend({
 	el: $('content'),
 	template: _.template(	'<div class="snapshot">'+
-												'<a href=""><%= subject%></a>'+
+												'<a href="#<%= id %>"><%= subject%></a>'+
 												'<article><%= body %></article>'+
 												'<p>By <%= author %> on <%= timestamp %></p>'+
 												'</div>'),
@@ -29,9 +29,45 @@ var BlogView = Backbone.View.extend({
 	}
 });
 
-var blog = new BlogModel({author: 'Jacky',subject: 'Virgin Galactic Crashed', body: 'Virigin Galactic\'s SpaceShipTwo rocket plane exploded and crashed during a test flight on Friday, killing one crew member and seriously injuring another, authorities said.', timestamp: '10/31/2014'})
-var blogview = new BlogView({model: blog})
-blogview.render();
+var BlogListSnapShot = Backbone.View.extend({
+	render: function(){
+		this.collection.forEach(this.addOne,this);
+	},
+	addOne: function(blog){
+		var snapshot = new BlogSnapShot({model: blog});
+		snapshot.render();
+	}
+});
+
+//testing article!
+var bloglist = new BlogList([{
+	author: 'Jacky',
+	subject: 'Virgin Galactic Crashed',
+	body: 'Virigin Galactic\'s SpaceShipTwo rocket plane exploded and crashed during a test flight on Friday, killing one crew member and seriously injuring another, authorities said.', 
+	timestamp: '10/31/2014',
+	id: 0
+},{
+	author: 'Jacky',
+	subject: 'Virgin Galactic Crashed',
+	body: 'Virigin Galactic\'s SpaceShipTwo rocket plane exploded and crashed during a test flight on Friday, killing one crew member and seriously injuring another, authorities said.', 
+	timestamp: '10/31/2014',
+	id: 1
+},{
+	author: 'Jacky',
+	subject: 'Virgin Galactic Crashed',
+	body: 'Virigin Galactic\'s SpaceShipTwo rocket plane exploded and crashed during a test flight on Friday, killing one crew member and seriously injuring another, authorities said.', 
+	timestamp: '10/31/2014',
+	id:2
+},{
+	author: 'Jacky',
+	subject: 'Virgin Galactic Crashed',
+	body: 'Virigin Galactic\'s SpaceShipTwo rocket plane exploded and crashed during a test flight on Friday, killing one crew member and seriously injuring another, authorities said.', 
+	timestamp: '10/31/2014',
+	id:3
+}]);
+var bloglistsnapshot = new BlogListSnapShot({collection: bloglist});
+bloglistsnapshot.render();
+//================================================================
 
 var headerView = new(Backbone.View.extend({
 	el: $('header'),
@@ -42,6 +78,20 @@ var headerView = new(Backbone.View.extend({
 		this.$el.html("<div class='icon'></div>"+
 									"<div class='logo'><h1>BlogBone<h1></div>"+
 									"<input type='text' placeholder='Search'>"+
-									"<div class='menu'></div>");
+									"<img src='./style/plus-5-256.png' />");
+	},
+	events: {
+		'mouseenter img': 'highlight',
+		'mouseleave img': 'nohighlight',
+		'click img': 'highlighted'
+	},
+	highlight: function(x){
+		$(x.currentTarget).addClass('highlight');
+	},
+	nohighlight: function(x){
+		$(x.currentTarget).removeClass('highlight');
+	},
+	highlighted: function(x){
+		$(x.currentTarget).toggleClass('highlighted');
 	}
 }));
